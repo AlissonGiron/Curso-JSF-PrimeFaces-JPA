2b1a2fe4759fdd1bc37a44e27fb9ddabd848772a
Alisson <Alisson@10.0.2.15>
2018-03-02 12:35:58 -0300

    Initial

---
+++ ProjetoMavenJSFPrimeFaces/src/main/java/sistema/modelos/Professor.java
@@ -0,0 +1,31 @@
+package sistema.modelos;
+
+public class Professor {
+	private int documento;
+	private String nome;
+	private String disciplina;
+		
+	public int getDocumento() {
+		return documento;
+	}
+	
+	public void setDocumento(int documento) {
+		this.documento = documento;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getDisciplina() {
+		return disciplina;
+	}
+	
+	public void setDisciplina(String disciplina) {
+		this.disciplina = disciplina;
+	}
+}
